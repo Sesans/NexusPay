9ad4f17b12d2d2ff68caad483687f55de553dbbc
leonardo <leonardopsantos95@gmail.com>
2026-02-24 12:22:46 -0300

    add transaction class

---
+++ src/main/java/com/nexuspay/ledger/internal/domain/model/Transaction.java
@@ -0,0 +1,35 @@
+package com.nexuspay.ledger.internal.domain.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID correlationId;
+
+    @Column(nullable = false, updatable = false)
+    private String description;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAT;
+
+    @Column(nullable = false)
+    private TransactionCondition status;
+
+    protected Transaction(){}
+
+    public Transaction(UUID correlationId, String description, LocalDateTime timestamp){
+        this.correlationId = correlationId;
+        this.description = description;
+        this.createdAT = timestamp;
+    }
+}
