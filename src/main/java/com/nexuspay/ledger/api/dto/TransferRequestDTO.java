e26f0a130010df265009cce154ff73a1776cb772
leonardo <leonardopsantos95@gmail.com>
2026-02-26 01:10:19 -0300

    added validation constraints in requestDTO

--- src/main/java/com/nexuspay/ledger/api/dto/TransferRequestDTO.java
+++ src/main/java/com/nexuspay/ledger/api/dto/TransferRequestDTO.java
@@ -1,12 +1,19 @@
 package com.nexuspay.ledger.api.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.util.UUID;
 
 public record TransferRequestDTO(
+        @NotNull(message = "Source account cannot be null")
         UUID sourceId,
+        @NotNull(message = "Destination account cannot be null")
         UUID destinationId,
+        @NotNull(message = "Correlation ID cannot be null")
         UUID correlationId,
         String description,
+        @Positive(message = "Amount must have a positive number")
         Long amount
 ) {
 }
