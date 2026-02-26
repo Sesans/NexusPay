7779883e23fbe4f3ab662bb602553df9233ce08f
leonardo <leonardopsantos95@gmail.com>
2026-02-26 01:12:18 -0300

    refactored facade to handle the returned transaction ID and create a ResponseDTO

--- src/main/java/com/nexuspay/ledger/LedgerFacade.java
+++ src/main/java/com/nexuspay/ledger/LedgerFacade.java
@@ -1,12 +1,16 @@
 package com.nexuspay.ledger;
 
 import com.nexuspay.ledger.api.dto.TransferRequestDTO;
+import com.nexuspay.ledger.api.dto.TransferResponseDTO;
 import com.nexuspay.ledger.internal.application.TransactionService;
+import com.nexuspay.ledger.internal.domain.model.TransactionCondition;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
 
 @Service
 @Validated
@@ -21,7 +25,14 @@ public class LedgerFacade {
             retryFor = {OptimisticLockingFailureException.class},
             backoff = @Backoff(delay = 100)
     )
-    public void transfer(TransferRequestDTO dto){
-        transactionService.processTransfer(dto);
+    public TransferResponseDTO transfer(TransferRequestDTO dto){
+        Long transactionId = transactionService.processTransfer(dto);
+
+        return new TransferResponseDTO(
+                transactionId,
+                dto.correlationId(),
+                TransactionCondition.PROCESSED,
+                LocalDateTime.now()
+        );
     }
 }
