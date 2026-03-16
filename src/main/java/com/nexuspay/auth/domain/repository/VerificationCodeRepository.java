46b6171c93ae6f63af8f0a969237fd8e8578f4c5
leonardo <leonardopsantos95@gmail.com>
2026-03-16 03:02:07 -0300

    added a findByUserId in VerificationCodeRepository to help find Codes related by Users

--- src/main/java/com/nexuspay/auth/domain/repository/VerificationCodeRepository.java
+++ src/main/java/com/nexuspay/auth/domain/repository/VerificationCodeRepository.java
@@ -3,5 +3,9 @@ package com.nexuspay.auth.domain.repository;
 import com.nexuspay.auth.domain.model.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByUserId(UUID uuid);
 }
