f1b760bcaac6c00bde0d07eee61e9ea3c5c86265
leonardo <leonardopsantos95@gmail.com>
2026-03-21 23:59:54 -0300

    feat: add the BusinessException to handle with business logic level exceptions

---
+++ src/main/java/com/nexuspay/ledger/domain/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.nexuspay.ledger.domain.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message){
+        super(message);
+    }
+}
