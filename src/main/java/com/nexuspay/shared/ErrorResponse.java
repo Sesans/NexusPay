422000bf6b14c626bb1864f962c22d6ca8b22a72
leonardo <leonardopsantos95@gmail.com>
2026-04-02 01:36:05 -0300

    feat: update the API response format
    The API returns only the information necessary for the user, avoiding the exposure of sensitive data, stack traces, or implementation details

--- src/main/java/com/nexuspay/shared/ErrorResponse.java
+++ src/main/java/com/nexuspay/shared/ErrorResponse.java
@@ -1,8 +1,11 @@
 package com.nexuspay.shared;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         String code,
         String message,
@@ -11,5 +14,9 @@ public record ErrorResponse(
 ) {
     public ErrorResponse(String code, String message) {
         this(code, message, LocalDateTime.now(), null);
+    }
+
+    public ErrorResponse(String code, String message, Map<String, Object> details) {
+        this(code, message, LocalDateTime.now(), details);
     }
 }
--- src/main/java/com/nexuspay/shared/GlobalExceptionHandler.java
+++ src/main/java/com/nexuspay/shared/GlobalExceptionHandler.java
@@ -1,56 +1,140 @@
 package com.nexuspay.shared;
 
-import com.nexuspay.ledger.domain.exception.AccountNotFoundException;
-import com.nexuspay.ledger.domain.exception.DuplicateTransactionException;
-import com.nexuspay.ledger.domain.exception.InsufficientBalanceException;
-import com.nexuspay.ledger.domain.exception.InvalidTransactionAmountException;
+import com.nexuspay.auth.domain.exception.DuplicateUserException;
+import com.nexuspay.auth.domain.exception.ExpiredCodeException;
+import com.nexuspay.auth.domain.exception.InvalidCodeException;
+import com.nexuspay.auth.domain.exception.InvalidCredentialsException;
+import com.nexuspay.ledger.domain.exception.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler{
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound(AccountNotFoundException exception){
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse("ACCOUNT_NOT_FOUND", exception.getMessage()));
+                .body(new ErrorResponse(
+                        "ACCOUNT_NOT_FOUND",
+                        exception.getMessage()
+                ));
     }
 
     @ExceptionHandler(DuplicateTransactionException.class)
     public ResponseEntity<ErrorResponse> handleDuplicate(DuplicateTransactionException exception){
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
-                .body(new ErrorResponse("DUPLICATE_TRANSACTION", exception.getMessage()));
+                .body(new ErrorResponse(
+                        "DUPLICATE_TRANSACTION",
+                        exception.getMessage()
+                ));
     }
 
     @ExceptionHandler(InsufficientBalanceException.class)
     public ResponseEntity<ErrorResponse> handleInsufficientBalance(InsufficientBalanceException exception){
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body(new ErrorResponse("INSUFFICIENT_FUNDS", exception.getMessage()));
+                .body(new ErrorResponse(
+                        "INSUFFICIENT_FUNDS",
+                        exception.getMessage()
+                ));
     }
 
     @ExceptionHandler(InvalidTransactionAmountException.class)
     public ResponseEntity<ErrorResponse> handleInvalidAmount(InvalidTransactionAmountException exception){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse("INVALID_AMOUNT", exception.getMessage()));
+                .body(new ErrorResponse(
+                        "INVALID_AMOUNT",
+                        exception.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateUser(DuplicateUserException exception){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse(
+                        "DUPLICATE_USER",
+                        exception.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(ExpiredCodeException.class)
+    public ResponseEntity<ErrorResponse> handleExpiredOTP(ExpiredCodeException exception){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        "EXPIRED_CODE",
+                        exception.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(InvalidCodeException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCodeSent(InvalidCodeException exception){
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(
+                        "INVALID_CODE",
+                        exception.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials() {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(
+                        "INVALID_CREDENTIALS",
+                        "Invalid CPF or password"
+                ));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleArgumentNotValid(MethodArgumentNotValidException exception){
+    public ResponseEntity<ErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, Object> details = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            details.put(error.getField(), error.getDefaultMessage());
+        });
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse("INVALID_ARGUMENT", exception.getMessage()));
+                .body(new ErrorResponse(
+                        "INVALID_ARGUMENT",
+                        "Invalid Request Data",
+                        details
+                ));
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException ex){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse(
+                        "CONFLICT",
+                        ex.getMessage()
+                ));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneral(Exception exception) {
+        log.error("Unexpected error: ", exception);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("INTERNAL_SERVER_ERROR", "An unexpected error occurred in our system with the message: " + exception));
+                .body(new ErrorResponse(
+                        "INTERNAL_SERVER_ERROR",
+                        "An internal server error occurred, try again later!"
+                ));
     }
 }
